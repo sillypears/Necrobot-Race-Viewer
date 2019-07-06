d5ef59a2255a2bdff1bb983e561b38cd99a1dd1b
sillypears <sillypears@users.noreply.github.com>
2019-07-06 13:01:03 -0400

    Bye bye debug

--- public/javascripts/races.js
+++ public/javascripts/races.js
@@ -8,10 +8,6 @@ $(function() {
 function formatTimestamp(t) {
     let offset = new Date().getTimezoneOffset()/60;
     let dt = new Date(t);
-
     dt.setHours(dt.getHours() - offset);
-
-    console.log(offset, dt)
-
     return moment(dt).format('LLLL');
 }
